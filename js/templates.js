62a6b12239f418f40de10badaaa693d99501dff7
Rex Lane <rex.m.lane@gmail.com>
2015-04-26 14:25:12 -0500

    updated styles
    updated styles

--- js/templates.js
+++ js/templates.js
@@ -4,6 +4,6 @@ template = Handlebars.compile($("#sections-template").html());
 
 function loadTemplates() {
 	var context_length = context.body[0].elements.length;
-	var random_entry = Math.floor((Math.random() * context_length) +1);
+	var random_entry = Math.floor((Math.random() * context_length) + 1);
   	$("#main").html(template(context.body[0].elements[random_entry]));
 }
