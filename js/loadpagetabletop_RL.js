1c376d8d126e09452020da3db6bcf1df47770556
Rex Lane <rex.m.lane@gmail.com>
2015-05-15 20:40:23 -0500

    dsfkjh
    sdkfjhg

--- js/loadpagetabletop_RL.js
+++ js/loadpagetabletop_RL.js
@@ -27,11 +27,13 @@ function initializeTabletopObject(dataSpreadsheet){
 // This function gets our data from our spreadsheet
 function pullDataFromTabletop(tabletopData, tabletop) {
   // console.log(tabletop.key);
+  // console.log(tabletopData);
   dataTabletop = tabletopData;
   // Go through our Google spreadsheet
   // Stored in tabletopData variable
   // tabletopData = Worksheet
   for (num in tabletopData) {
+    console.log(num);
     context["body"].push(tabletopData[num]);
   }
   // If you want to see what context looks like:
@@ -43,3 +45,28 @@ function pullDataFromTabletop(tabletopData, tabletop) {
 };
 
 
+template = Handlebars.compile($("#sections-template").html());
+
+
+
+function loadTemplates() {
+  var context_length = context.body[0].elements.length;
+  var random_entry = Math.floor((Math.random() * context_length) +1);
+    $("#content").html(template(context.body[0].elements[random_entry]));
+};
+
+// function showExample () {
+//  $("#example-chunk").removeClass("hidden");
+// };
+
+// function showDef() {
+//  $("#def-chunk").removeClass("hidden");
+// };
+
+function showExample() {
+  $("#example-chunk").toggle();
+}
+
+function showDef() {
+  $("#def-chunk").toggle();
+};
