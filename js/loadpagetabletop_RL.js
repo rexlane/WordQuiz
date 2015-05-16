39d09f4c984bb56496166c1b75ec238b21bd5b38
Rex Lane <rex.m.lane@gmail.com>
2015-05-15 21:11:46 -0500

    commit
    commit

--- js/loadpagetabletop_RL.js
+++ js/loadpagetabletop_RL.js
@@ -11,6 +11,7 @@ jqueryNoConflict(document).ready(function(){
 //  RL's spreadsheet
   initializeTabletopObject('https://docs.google.com/spreadsheets/d/1ZgC8FlfyqXXJUYJB4B0fWvPxOGQoMTOnoC2bXnTdhao/pubhtml');
 // https://docs.google.com/spreadsheets/d/1ZgC8FlfyqXXJUYJB4B0fWvPxOGQoMTOnoC2bXnTdhao/pubhtml
+  template = Handlebars.compile($("#sections-template").html());
 });
 
 // Pull data from Google spreadsheet
@@ -27,13 +28,12 @@ function initializeTabletopObject(dataSpreadsheet){
 // This function gets our data from our spreadsheet
 function pullDataFromTabletop(tabletopData, tabletop) {
   // console.log(tabletop.key);
-  // console.log(tabletopData);
+
   dataTabletop = tabletopData;
   // Go through our Google spreadsheet
   // Stored in tabletopData variable
   // tabletopData = Worksheet
   for (num in tabletopData) {
-    console.log(num);
     context["body"].push(tabletopData[num]);
   }
   // If you want to see what context looks like:
@@ -45,14 +45,16 @@ function pullDataFromTabletop(tabletopData, tabletop) {
 };
 
 
-template = Handlebars.compile($("#sections-template").html());
+
 
 
 
 function loadTemplates() {
   var context_length = context.body[0].elements.length;
+  
   var random_entry = Math.floor((Math.random() * context_length) +1);
     $("#content").html(template(context.body[0].elements[random_entry]));
+    console.log(template(context.body[0].elements[random_entry]));
 };
 
 // function showExample () {
